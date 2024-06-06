dae3b873087d95e5b706a9a125c0df78de8f0392
DongningLi <26728039+DongningLi@users.noreply.github.com>
2024-06-06 22:47:49 +1000

    Create basic post api

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# myapp/views.py
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework import status
+import pandas as pd
+
+class FileUploadView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
+    def post(self, request, *args, **kwargs):
+        file_obj = request.FILES['file']
+        try:
+            df = pd.read_csv(file_obj)
+            data = df.to_dict(orient='records')
+            return Response(data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp.views import FileUploadView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('upload/', FileUploadView.as_view(), name='file-upload'),
 ]
+

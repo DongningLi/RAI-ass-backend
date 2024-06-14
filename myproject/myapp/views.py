25bc8f15159327fcaf72fe3077adb4719a9d4bfc
DongningLi <26728039+DongningLi@users.noreply.github.com>
2024-06-14 10:53:12 +1000

    save type and col content to mongodb and response

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,46 +1,17 @@
 import json
-from django.apps import apps
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 import pandas as pd
-
+import uuid
+import json
 from core.infer_data_types import detect_type, infer_type
-from .models import ColsTypes, FileContent, FileContentCollection
 
 
-def save_type_instance(column_types):
+from .models import ColsTypeCollection, FileContentCollection
 
-    try:
-        cols_types = ColsTypes (
-            name = column_types["Name"],
-            birthdate = column_types["Birthdate"],
-            score = column_types["Score"],
-            grade = column_types["Grade"]
-        )
 
-        cols_types.save()
 
-    except Exception as e:
-        print ("save type fails:", e)
-
-def save_content_instance(df):
-
-    try:
-        for _, row in df.iterrows():
-
-            content = FileContent(
-                name = row["Name"],
-                birthdate = row["Birthdate"],
-                score = row["Score"],
-                grade = row["Grade"],
-            )
-
-            content.save()
-    except Exception as e:
-        print ("save content fails:", e)
-
-        
 class FileUploadView(APIView):
 
     def post(self, request, *args, **kwargs):
@@ -48,28 +19,19 @@ class FileUploadView(APIView):
 
         try:
             df = pd.read_csv(file_obj)
-
-            data_content = df.to_dict(orient='records')
             column_types = detect_type(df)
-            df = infer_type(df,column_types)
-
-            FileContentCollection.insert_one(column_types)
-
-
-            # print('------------------1-------------------------')
-
-            # DynamicEmployeeModel = create_dynamic_model('DynamicEmployeeModel', column_types)
-            # print('-----------------2--------------------------')
-            # apps.register_model('myapp', DynamicEmployeeModel)
-
-            # print('------------------3-------------------------')
-            # instance = DynamicEmployeeModel(Name="John Doe", Birthdate="1/01/1991", Score=89, Grade="A")
-            # print('------------------4-------------------------')
-            # instance.save()
-
-
-            # save_type_instance(column_types)
-            # save_content_instance(data_content)
+            file_id = uuid.uuid4()
+            df['fileId'] = str(file_id)
+            data_content = df.to_dict(orient='records')
+            column_types['fileId'] = str(file_id)
+            FileContentCollection.insert_many(data_content)
+            ColsTypeCollection.insert_one(column_types)
+            
+            # convert objectId to str for json response
+            for record in data_content:
+                if '_id' in record:
+                    record['_id'] = str(record['_id'])
+            column_types['_id'] = str(column_types['_id'])
 
             data = {"contents":data_content,
                     "types":column_types}

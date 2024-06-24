64a1f866974b4e4084503f6aa72e96617c8abc8a
DongningLi <26728039+DongningLi@users.noreply.github.com>
2024-06-24 13:58:28 +1000

    update core func

--- myproject/core/infer_data_types.py
+++ myproject/core/infer_data_types.py
@@ -1,3 +1,4 @@
+import re
 import numpy as np
 import pandas as pd
 from requests import Response
@@ -19,75 +20,129 @@ def can_convert_to_int(df_sample_col):
     # Drop NA values and check if any remaining value is a float
     return df_sample_col.dropna().apply(lambda x: float(x).is_integer()).all()
 
+# detect if a number could be converted to timedelta
+def convert_single_duration(duration):
+    
+    match = re.match(r'(\d+)\s*(days?|hours?|minutes?|seconds?|weeks?|months?|years?)', duration)
+
+    if not match:
+        return np.nan
+    value, unit = match.groups()
+    value = int(value)
+    
+    # Convert to the corresponding timedelta format
+    if unit == 'days':
+        return pd.to_timedelta(value, unit='days')
+    elif unit == 'hours':
+        return pd.to_timedelta(value, unit='hours')
+    elif unit == 'minutes':
+        return pd.to_timedelta(value, unit='minutes')
+    elif unit == 'seconds':
+        return pd.to_timedelta(value, unit='seconds')
+    elif unit == 'weeks':
+        return pd.to_timedelta(value, unit='W')
+    elif unit == 'months':
+        # Approximate a month as 30 days
+        return pd.to_timedelta(value * 30, unit='days')
+    elif unit == 'years':
+        # Approximate a year as 365 days
+        return pd.to_timedelta(value * 365, unit='days')
+    else:
+        return np.nan
+    
+
 # detect possible type
-def detect_col_type(df_sample_col, category_threshold):
-
-    # Attempt to convert to complex
-    try:
-        df_converted = df_sample_col.str.replace("i","j").apply(complex)
-        if not df_converted.isna().all(): 
-           return "complex"
-    except (ValueError, TypeError):
-        pass
-
-    # Attempt to convert to numeric
-    try:
-        df_converted = pd.to_numeric(df_sample_col, errors='coerce')
-        if not df_converted.isna().all():
-            if can_convert_to_int(df_converted):
-                return "int64"
-            else:
-                return "float64"
-    except (ValueError, TypeError):
-        pass
-
-    # Attempt to convert to datetime
-    try:
-        df_converted = pd.to_datetime(df_sample_col, errors='coerce')
-        if not df_converted.isna().all():
-            return 'datetime64[ns]'
-    except (ValueError, TypeError):
-        pass
-
-    # Attempt to convert to timedelta
-    try:
-        df_converted = pd.to_timedelta(df_sample_col, errors='coerce')
-        if not df_converted.isna().all():
-            return 'timedelta64[ns]'
-    except (ValueError, TypeError):
-        pass
+def detect_col_type(df_sample_col, category_threshold, belief_threshold):
+    
+    # Check if col should be boolean
+    if df_sample_col.dropna().isin([True, False, 'True', 'False', 'TRUE', 'FALSE', 'yes', 'no', 'T', 'F', 't', 'f', '1', '0']).all():
+        return "bool"
 
     # Check if the column should be categorical
     if len(df_sample_col.unique()) / len(df_sample_col) < category_threshold:
         return "category"
 
-    # Check if col should be boolean
-    if df_sample_col.dropna().isin([True, False, 'True', 'False']).all():
-        return "bool"
+
+    # Attempt to convert to numeric
+    try:
+        df_converted = pd.to_numeric(df_sample_col.str.replace(',', ''), errors='raise')
+        if df_converted.isna().sum().sum() <= belief_threshold * df_converted.size:
+            if can_convert_to_int(df_converted):
+                return "int64"
+            else:
+                return "float64"
+    except :
+        try:
+            df_converted = pd.to_numeric(df_sample_col, errors='raise')
+            if df_converted.isna().sum().sum() <= belief_threshold * df_converted.size:
+                if can_convert_to_int(df_converted):
+                    return "int64"
+                else:
+                    return "float64"
+        except:
+            pass
+
+    # Attempt to convert to complex
+    try:
+        df_converted = df_sample_col.apply(complex)
+        if df_converted.isna().sum().sum() <= belief_threshold * df_converted.size:
+           return "complex"
+    except :
+        pass
+
+    # Attempt to convert to datetime
+    try:
+        df_converted = pd.to_datetime(df_sample_col, errors='coerce')
+        if df_converted.isna().sum().sum() <= belief_threshold * df_converted.size:
+           return 'datetime64[ns]'
+    except:
+        pass
+        
+    # Attempt to convert to timedelta
+    try:
+        df_converted= df_sample_col.apply(convert_single_duration)
+        if df_converted.isna().sum().sum() <= belief_threshold * df_converted.size:
+            return 'timedelta64[ns]'
+    except :
+        pass
             
     return "object"
+
 
 def detect_type(df_sample):
 
     category_threshold = 0.5
+    belief_threshold = 0.5
     # detect type of column from sample
-    column_types = {col: detect_col_type(df_sample[col], category_threshold) for col in df_sample.columns}
+    column_types = {col: detect_col_type(df_sample[col], category_threshold, belief_threshold) for col in df_sample.columns}
     
     return column_types
 
 #assign type 
 def infer_type(df,column_types):
+
     for col in df.columns:
+        if (column_types[col] == "bool"):
+            df[col] = df[col].replace({'true': True, 'false': False, 'yes': True, 'no': False, 't': True, 'f': False, '1': True, '0': False, 'T': True, 'F': False,})
+            df[col] = df[col].astype(column_types[col])
+            continue
+
+        if (column_types[col] == "timedelta64[ns]" or "complex"):
+            continue
+    
         try:
-            # replace 'Not Available' with np.nan
-            df[col] = df[col].replace('Not Available', np.nan).astype(column_types[col])
+            df[col] = df[col].str.astype(column_types[col])
             continue
         except:
+
             try:
-                # replace the imaginary i with j for complex numbers
-                df[col] = df[col].str.replace("i","j").astype(column_types[col])
+                df[col] = df[col].str.replace(',', '').fillna(0).astype(column_types[col])
                 continue
-            except Exception as e:
-                return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            except:
+                try:
+                    df[col] = df[col].apply(convert_single_duration)
+                    continue
+                except Exception as e:
+                    return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
     return df
